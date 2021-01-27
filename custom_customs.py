36c09744b3330c772886e8c696e0afd3f08eed75
Ben Hiett <ben@hiett.co.uk>
2021-01-27 12:06:12 +0000

    updated with python 3.9 specific type hints

--- custom_customs.py
+++ custom_customs.py
@@ -3,12 +3,12 @@
 ''' https://adventofcode.com/2020/day/6 '''
 
 
-def part1(lines: list) -> int:
+def part1(lines: list[str]) -> int:
     
     return sum([len(set(g.replace('\n', ''))) for g in ''.join(lines).split('\n\n')])
 
 
-def part2(lines: list) -> int:
+def part2(lines: listo[str]) -> int:
 
     group_strings = [g.split('\n') for g in ''.join(lines).split('\n\n')]
     return sum([len(set.intersection(*map(set, g))) for g in group_strings])
